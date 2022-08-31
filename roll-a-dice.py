f51f32e18d784b3ffbdeb57fc24a01bcfbb234bb
HorusXenoverse <92953478+HorusXenoverse@users.noreply.github.com>
2022-08-31 16:10:33 -0500

    Add files via upload

---
+++ roll-a-dice.py
@@ -0,0 +1,17 @@
+import random
+from urllib import response
+
+response  = input("Desea tirar un dado? Y o N")
+while response == "y":
+    no = random.randint(1,6)
+    if no == 1:
+        print("[-----]")
+        print("[     ]")
+        print("[  0  ]")
+        print("[     ]")
+        print("[-----]")
+    
+      
+
+     
+
